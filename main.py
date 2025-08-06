293b7c2b4e7c2e0cbf6c38a5e002c981f20997e4
Paurakh <pandeypaurakhraj@gmail.com>
2025-08-06 12:41:54 +0545

    mounting 2nd server endpoint

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ import contextlib
 from fastapi import FastAPI
 import os
 from src.officer import officer_mcp
+from src.homestay import homestay_mcp
 
 # Create a combined lifespan to manage both session managers
 @contextlib.asynccontextmanager
 async def lifespan(app: FastAPI):
     async with contextlib.AsyncExitStack() as stack:
         await stack.enter_async_context(officer_mcp.session_manager.run())
+        await stack.enter_async_context(homestay_mcp.session_manager.run())
         yield
-
 
 app = FastAPI(lifespan=lifespan)
 app.mount("/officer", officer_mcp.streamable_http_app())
-
-
+app.mount("/homestay", homestay_mcp.streamable_http_app())
 
 PORT = os.environ.get("PORT", 8080)
 
